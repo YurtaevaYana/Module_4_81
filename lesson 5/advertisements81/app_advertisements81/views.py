7ea2e818d7e619eca81e0f582a5c28b21e491a1c
Yana <yurtaeva.yana@gmail.com>
2023-07-15 13:16:12 +0300

    cheatsheet les5

---
+++ lesson 5/advertisements81/app_advertisements81/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
